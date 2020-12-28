81d618bcee68c3647d11ed9bf3b27e497fca7f31
maazil3861 <maazshaikh7020@gmail.com>
2020-12-28 16:37:56 +0530

    Identifiers and Keywords

---
+++ code/src/Identifiers and Keywords/identifier.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	i := 10
+	fmt.Println("identifer ", i)
+
+	//  compile time error program not execute.
+	// func := 20
+	// fmt.Println("identifer ", func)
+}
