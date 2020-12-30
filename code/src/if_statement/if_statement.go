748da6135a5a3d94493019a2650045bfc2fe479f
maazil3861 <maazshaikh7020@gmail.com>
2020-12-30 12:19:47 +0530

    if statement program

---
+++ code/src/if_statement/if_statement.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var no int = 10
+
+	if no%2 == 0 {
+		fmt.Print("no is even number \n")
+	}
+	fmt.Print("This program is use for if statement \n")
+}
