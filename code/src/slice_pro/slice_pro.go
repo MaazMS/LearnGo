4297d26830d7d694c4fd1c47153dfef60c2023e9
maazil3861 <maazshaikh7020@gmail.com>
2021-01-01 11:47:53 +0530

    slice program

---
+++ code/src/slice_pro/slice_pro.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var s1 = []string{"my", "name", "is", "Maaz"}
+
+	var noeven = []int{2, 4, 6, 8, 10}
+
+	fmt.Println(s1)
+	fmt.Println(noeven)
+
+}
